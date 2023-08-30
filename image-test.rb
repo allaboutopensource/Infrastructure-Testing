a97b34cd67dbf4de09b5d3343651bad1468e17f1
Sunil kathait <5890851+allaboutopensource@users.noreply.github.com>
2023-08-30 22:13:48 +0530

    Create inspec test cases
    inspec tool test cases for the packer images

---
+++ image-test.rb
@@ -0,0 +1,42 @@
+# packer_image_test.rb
+
+# Test the Packer-built CentOS 8 image
+
+control 'centos-8-image' do
+  impact 1.0
+  title 'CentOS 8 Image Configuration testing'
+  desc 'Ensure CentOS 8 image has the required configurations'
+
+  # Check if required packages are installed
+  describe package('falcon-sensor') do
+    it { should be_installed }
+  end
+
+  describe package('BESAgent') do
+    it { should be_installed }
+  end
+
+  # Check if required ports are open
+  describe port(22) do
+    it { should be_listening }
+    its('protocols') { should include 'tcp' }
+  end
+  # Check if specific user exist exist
+  describe user('devops') do
+    it { should exist }
+  end
+
+  describe user('devops') do
+    it { should exist }
+  end
+
+  # Check if specific files and directories exist
+  describe file('/etc/hosts') do
+    it { should exist }
+    its('content') { should include 'localhost' }
+  end
+
+  describe directory('/var/log') do
+    it { should exist }
+    it { should be_directory }
+  end
